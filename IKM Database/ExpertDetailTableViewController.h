e0338ab7398a9113dc3fc481dc191f7c26d69120
Michael Mühlebach <michael@anduin.ch>
2012-01-23 16:27:41 +0100

    New Controller for Expert Details.

---
+++ IKM Database/ExpertDetailTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ExpertDetailTableViewController.h
+//  skilldb
+//
+//  Created by Michael Mühlebach on 1/23/12.
+//  Copyright (c) 2012 Zühlke Engineering AG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ExpertDetailTableViewController : UITableViewController
+
+@end
